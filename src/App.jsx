5e7dfb68fa3b5eceb7ba89dcbae3924adbd689d8
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-08-26 16:36:48 +0200

    Creazione e importazione del main header footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-  return <Header />;
+  return (
+  <>
+   <Header />
+   <Main />
+   <Footer />
+  </>
+  );
 }
 
 export default App
