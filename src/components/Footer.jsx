787c11f4ab1dee9f22bcaa7bc99e157495dc92fd
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-08-26 19:32:16 +0200

    middle footer finico e decorato

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,7 +30,54 @@ const Footer = () => {
       <div className="middle-footer">
         <div className="container">
           <div className="row">
-            <div className="col-50"></div>
+            <div className="col-50 row">
+               <div className="column col-25 padding-20">
+            <h3>DC COMICS</h3>
+            <ul>
+              <li>Characters</li>
+              <li>Comics</li>
+              <li>Movies</li>
+              <li>TV</li>
+              <li>Games</li>
+              <li>Videos</li>
+              <li>News</li>
+            </ul>
+
+            <h3>SHOP</h3>
+            <ul>
+              <li>Shop DC</li>
+              <li>Shop DC Collectibles</li>
+            </ul>
+          </div>
+
+          <div className="column col-25 padding-20">
+            <h3>DC</h3>
+            <ul>
+              <li>Terms Of Use</li>
+              <li>Privacy Policy (New)</li>
+              <li>Ad Choices</li>
+              <li>Advertising</li>
+              <li>Jobs</li>
+              <li>Subscriptions</li>
+              <li>Talent Workshops</li>
+              <li>CPSC Certificates</li>
+              <li>Ratings</li>
+              <li>Shop Help</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+
+              <div className="column col-25 padding-20">
+            <h3>SITES</h3>
+            <ul>
+              <li>DC</li>
+              <li>MAD Magazine</li>
+              <li>DC Kids</li>
+              <li>DC Universe</li>
+              <li>DC Power Visa</li>
+            </ul>
+          </div>
+        </div>
             <div className="col-50 background-dc"></div>
           </div>
         </div>
