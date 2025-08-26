af6816a897527cedaf0a995ed9030273d4c6e4fa
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-08-26 18:09:10 +0200

    Creazione banner blue con img

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,29 @@ import React from 'react'
 const Footer = () => {
   return <footer>
     <div className="blue-banner">
-      
+      <div className="container">
+        <div className="row">
+          <div className="col-100">
+            <div className="icons-list">
+              <div><img src="./img/buy-comics-digital-comics.png" alt="" />
+              <span>DIGITAL COMICS</span>
+              </div>
+              <div><img src="./img/buy-comics-merchandise.png" alt="" />
+              <span>DC MERCHANDISE</span>
+              </div>
+              <div><img src="./img/buy-comics-subscriptions.png" alt="" />
+              <span>SUBSCRIPTION</span>
+              </div>
+              <div><img src="./img/buy-comics-shop-locator.png" alt="" />
+              <span>COMIC SHOP LOCATOR</span>
+              </div>
+              <div><img src="./img/buy-dc-power-visa.svg" alt="" />
+              <span>DC POWER VISA</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
       <div className="middle-footer"></div>
       <div className="bottom-footer"></div>
